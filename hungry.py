76c01830f831e6dd66659d5c365a4fa6f7fe5f80
MinkeshGupta <Minkesh2212@gmail.com>
2020-07-11 17:07:57 +0530

    changes

--- hungry.py
+++ hungry.py
@@ -1,5 +1,8 @@
 hungry = input("are you hungry?")
 if hungry == "yes":
     print("eat Samosa")
+    print("eat pizza")
+    print("eat alot")
 else:
     print("Do Homework")
+    print("sleep")
---
+++ original.py
@@ -0,0 +1,5 @@
+hungry = input("are you hungry?")
+if hungry == "yes":
+    print("eat Samosa")
+else:
+    print("Do Homework")
